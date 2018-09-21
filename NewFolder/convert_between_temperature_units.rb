0c2022f28cbafc351c77e5cbed8b5d010dfbd553
Dimitar Stoilov <dks@aeneas.skyaffie.com>
2018-09-21 16:38:33 +0300

    convert_between_temperature_units.rb

---
+++ NewFolder/convert_between_temperature_units.rb
@@ -0,0 +1,35 @@
+
+
+
+
+
+def convert_between_temperature_units(x, a, b)
+	if a == 'C' && b == 'K'
+		x = x + 273.15
+
+	elsif a == 'C' && b == 'F'
+		x = 32.00 + x*1.80
+
+	elsif a == 'F' && b == 'K'
+		x = (x - 32.00)/1.80 + 273.15
+
+	elsif a == 'F' && b == 'C'
+		x =  (x-32.00)/1.80
+
+	elsif a == 'K' && b == 'C'
+		x = x - 273.15
+
+	elsif a == 'K' && b == 'F'
+		x = (x + 32.00)*1.80 - 273.15
+
+	elsif a == b
+		x
+
+	end
+
+end
+
+
+puts convert_between_temperature_units(1, 'C', 'C')
+
+
